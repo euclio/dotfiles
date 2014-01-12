53b1795d10f9b019f41f7daefad460b23222594b
Andy Russell <arussell123@gmail.com>
2014-01-12 15:07:52 -0800

    don't tile fullscreen windows

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,4 +1,6 @@
 import XMonad
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
 
 main = do
     xmonad $ defaultConfig
@@ -6,8 +8,13 @@ main = do
         , modMask            = mod4Mask
         , normalBorderColor  = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
+        , handleEventHook    = fullscreenEventHook
+        , manageHook         = myManageHook
         }
 
 myTerminal = "urxvt -sl 0 -e bash -c \"tmux new-session\""
 myNormalBorderColor = "#d0d0d0"
 myFocusedBorderColor = "#ffa500"
+
+myManageHook = isFullscreen --> doFullFloat
+
