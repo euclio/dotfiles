23944ee548e06e072e26214d462947e7c6875b3d
Andy Russell <arussell123@gmail.com>
2014-02-26 15:57:58 -0800

    don't draw borders around single window

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
 
 main = do
     xmonad $ defaultConfig
@@ -10,11 +11,13 @@ main = do
         , focusedBorderColor = myFocusedBorderColor
         , handleEventHook    = fullscreenEventHook
         , manageHook         = myManageHook
+        , layoutHook         = myLayoutHook
         }
 
 myTerminal = "urxvt -sl 0 -e bash -c \"tmux new-session\""
 myNormalBorderColor = "#d0d0d0"
 myFocusedBorderColor = "#ffa500"
+myLayoutHook = smartBorders $ layoutHook defaultConfig
 
 myManageHook = isFullscreen --> doFullFloat
 
