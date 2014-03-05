a48791390d3658fc8386b1d6a9f55073865837c1
Andy Russell <arussell123@gmail.com>
2014-03-04 21:20:28 -0800

    add xmobar

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,23 +1,24 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
 
-main = do
-    xmonad $ defaultConfig
-        { terminal           = myTerminal
-        , modMask            = mod4Mask
-        , normalBorderColor  = myNormalBorderColor
-        , focusedBorderColor = myFocusedBorderColor
-        , handleEventHook    = fullscreenEventHook
-        , manageHook         = myManageHook
-        , layoutHook         = myLayoutHook
-        }
+main = xmonad =<< xmobar myConfig
+
+myConfig = defaultConfig
+    { terminal           = myTerminal
+    , modMask            = mod4Mask
+    , normalBorderColor  = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
+    , handleEventHook    = fullscreenEventHook
+    , manageHook         = myManageHook
+    , layoutHook         = myLayoutHook
+    }
 
 myTerminal = "urxvt -sl 0 -e bash -c \"tmux new-session\""
 myNormalBorderColor = "#d0d0d0"
 myFocusedBorderColor = "#ffa500"
 myLayoutHook = smartBorders $ layoutHook defaultConfig
-
-myManageHook = isFullscreen --> doFullFloat
-
+myManageHook = (isFullscreen --> doFullFloat) <+> manageHook defaultConfig <+> manageDocks
