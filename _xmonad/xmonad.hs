f27cab0bf3a27044186031e40724b10e6760ba85
Andy Russell <arussell123@gmail.com>
2014-03-05 18:26:14 -0800

    remove layout display from xmobar

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -4,21 +4,27 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
+import XMonad.Util.Run
 
-main = xmonad =<< xmobar myConfig
-
-myConfig = defaultConfig
-    { terminal           = myTerminal
-    , modMask            = mod4Mask
-    , normalBorderColor  = myNormalBorderColor
-    , focusedBorderColor = myFocusedBorderColor
-    , handleEventHook    = fullscreenEventHook
-    , manageHook         = myManageHook
-    , layoutHook         = myLayoutHook
-    }
+main = do
+    h <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+    xmonad $ defaultConfig
+        { terminal           = myTerminal
+        , modMask            = mod4Mask
+        , normalBorderColor  = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+        , handleEventHook    = fullscreenEventHook
+        , manageHook         = myManageHook
+        , layoutHook         = myLayoutHook
+        , logHook            = myLogHook h
+        }
 
 myTerminal = "urxvt"
 myNormalBorderColor = "#d0d0d0"
 myFocusedBorderColor = "#ffa500"
-myLayoutHook = smartBorders $ layoutHook defaultConfig
+myLayoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
 myManageHook = (isFullscreen --> doFullFloat) <+> manageHook defaultConfig <+> manageDocks
+myLogHook h = dynamicLogWithPP $ xmobarPP
+    {   ppOutput = hPutStrLn h
+    ,   ppOrder = \(ws:_:t:_) -> [ws,t]
+    }
