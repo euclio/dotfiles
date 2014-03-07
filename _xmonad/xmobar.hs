f072d7eaf5f0656b4c500c77108707a195f7f22f
Andy Russell <arussell123@gmail.com>
2014-03-07 03:12:31 -0800

    add battery status

--- _xmonad/xmobar.hs
+++ _xmonad/xmobar.hs
@@ -6,9 +6,10 @@ Config { font = "-*-Fixed-Bold-R-Normal-*-13-*-*-*-*-*-*-*"
                     , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Date "%a %b %_d %l:%M" "date" 10
+                    , Run Battery ["-t","Batt: <left>% (<timeleft>)"] 10
                     , Run StdinReader
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu% | %memory%     <fc=#ee9a00>%date%</fc> | %KONT%"
+       , template = "%StdinReader% }{ %battery% | %cpu% | %memory%     <fc=#ee9a00>%date%</fc> | %KONT%"
        }
