43c84769a68b01c3c9fdcbd2878f37dc6e742bfa
Andy Russell <arussell123@gmail.com>
2014-03-13 20:21:34 -0700

    check BAT0, AC0 explicitly

--- _xmonad/xmobar.hs
+++ _xmonad/xmobar.hs
@@ -6,8 +6,10 @@ Config { font = "-*-Fixed-Bold-R-Normal-*-13-*-*-*-*-*-*-*"
                     , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Date "%a %b %_d %l:%M" "date" 10
-                    , Run Battery ["-t", "Batt: <left>% (<timeleft>)<acstatus>",
-                                   "--", "-O", " <fc=yellow>⚡</fc>",
+                    , Run BatteryP ["BAT0"]
+                                   ["-t", "Batt: <left>% (<timeleft>)<acstatus>",
+                                   "--", "-f", "AC0/online",
+                                   "-O", " <fc=yellow>⚡</fc>",
                                    "-i", "", "-o", ""] 10
                     , Run Com "/bin/bash" ["-c", "~/dotfiles/scripts/get_volume.sh"] "vol" 10
                     , Run StdinReader
