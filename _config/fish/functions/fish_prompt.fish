9cd99599748dba97e7ba2b6cec5ec9b46f508c84
Andy Russell <arussell123@gmail.com>
2014-04-01 23:31:18 -0700

    use new prompt for fish
        Show exit status and put the prompt on the second line.

--- _config/fish/functions/fish_prompt.fish
+++ _config/fish/functions/fish_prompt.fish
@@ -1,5 +1,15 @@
 function fish_prompt
-    set_color $fish_color_cwd
-    echo -n (prompt_pwd)
-    echo -n '> '
+    # Remember the color to use for the status
+    if [ $status -eq 0 ]
+        set status_color green --bold
+    else
+        set status_color red --bold
+    end
+    set_color $status_color; printf '┌ '
+    set_color normal; printf '%s@%s' (whoami) (hostname)
+    set_color normal; printf ':';
+    set_color yellow; printf '%s' (prompt_pwd)
+    echo
+    set_color $status_color; printf '└╌╌┄┄ ❯❯ '
+    set_color normal
 end
