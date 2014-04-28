111e5ba06f1179af218d6ea726a23a047fcf3d64
Andy Russell <arussell123@gmail.com>
2014-04-28 01:10:22 -0700

    activate vi-mode in fish_user_key_bindings
    Enabling the key bindings in config.fish would cause the shell to lock on
    startup.

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -10,9 +10,6 @@ set -x MANPAGER "/bin/sh -c \"col -b |                                  \
         -c 'set ft=man ts=8 nolist noru nonu nornu noma nosc tw=0 ls=0' \
         -\""
 
-# Activate vi-mode
-fish_vi_key_bindings
-
 # Enable autocomplete in Python interpreter
 set -x PYTHONSTARTUP ~/.pythonrc
 
