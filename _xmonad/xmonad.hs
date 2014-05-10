4bedd13e651d8ebb2b3e2c9a4852797d256a0de1
Andy Russell <arussell123@gmail.com>
2014-05-09 20:33:47 -0700

    fix compiler warnings
    Added the 'remindWhen' field to the UrgencyConfig, and added type signatures to
    all top level bindings. Additional warnings are ignored (particularly for the
    layoutHook) because the types are too complicated for the warnings to be
    useful.

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,3 +1,8 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
+import Data.Monoid
+import GHC.IO.Handle.Types
+
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
@@ -10,14 +15,19 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Paste
 import XMonad.Util.Run
 
+myTerminal :: String
 myTerminal              = "urxvt -e fish"
+
+myNormalBorderColor :: String
 myNormalBorderColor     = "#d0d0d0"
+
+myFocusedBorderColor :: String
 myFocusedBorderColor    = "#ffa500"
 
 myLayoutHook            = smartBorders $ avoidStruts $
                           layoutHook defaultConfig
 
-
+myManageHook :: Query (Endo WindowSet)
 myManageHook = composeAll . concat $
     [ [ isFullscreen --> doFullFloat ]
     , [ appName =? c --> (placeHook chatPlacement <+> doFloat) | c <- myChatApps ]
@@ -28,16 +38,20 @@ myManageHook = composeAll . concat $
         [ "crx_nckgahadagoaajjgafhacjanaoiihapd" -- Chrome Hangouts Extension
         ]
 
+chatPlacement :: Placement
 chatPlacement = withGaps (0, 0, 22, 0) (inBounds (smart (1, 1)))
 
+myLogHook :: Handle -> X()
 myLogHook h = dynamicLogWithPP $ xmobarPP
     {   ppOutput = hPutStrLn h
     ,   ppOrder = \(ws:_:t:_) -> [ws,t]
     ,   ppUrgent = xmobarColor "yellow" "red"
     }
 
+myUrgentConfig :: UrgencyConfig
 myUrgentConfig = UrgencyConfig
     {   suppressWhen = OnScreen
+    ,   remindWhen   = Dont
     }
 
 main :: IO()
