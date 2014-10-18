c46394409b10d79a7f41203e3555f57b8deba2ba
Andy Russell <arussell123@gmail.com>
2014-10-18 14:40:15 -0700

    show current vi mode in fish prompt

--- _config/fish/functions/fish_prompt.fish
+++ _config/fish/functions/fish_prompt.fish
@@ -1,21 +1,34 @@
 function fish_prompt
+    set n (set_color normal)             # Normal color
+    set ve (set_color magenta)           # Virtualenv color
+    set wd (set_color yellow)            # Working directory color
+
     # Remember the color to use for the status
     if [ $status -eq 0 ]
-        set status_color green --bold
+        set st (set_color green --bold)      # Status success
     else
-        set status_color red --bold
+        set st (set_color red --bold)        # Status error code
     end
-    set_color $status_color; printf '┌ '
-    set_color normal; printf '%s@%s' (whoami) (hostname)
-    set_color normal; printf ':';
-    set_color yellow; printf '%s' (prompt_pwd)
+
+    # Remember the color to use for which vim mode we're in.
+    # We use the same colors that my vim config uses.
+    switch $fish_bind_mode
+    case default
+        set vi (set_color aeee00)
+        set mode 'N'
+    case insert
+        set vi (set_color 0a9dff)
+        set mode 'I'
+    case visual
+        set vi (set_color ffa724)
+        set mode 'V'
+    end
+
+    printf "$st┌ $n"
+    printf "%s@%s:$wd%s$n" (whoami) (hostname) (prompt_pwd)
     if set -q VIRTUAL_ENV
-        printf ' '
-        set_color magenta
-        printf '(%s)' (basename "$VIRTUAL_ENV")
-        set_color normal;
+        printf " $ve(%s)$n" (basename "$VIRTUAL_ENV")
     end
     echo
-    set_color $status_color; printf '└╌╌┄┄ ❯❯ '
-    set_color normal
+    printf "$st└╌╌┄┄ $vi❯❯$n " $mode
 end
