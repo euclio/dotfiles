784a8397f55583e559e87ca7efbaeb5293780c32
Andy Russell <arussell123@gmail.com>
2014-11-20 17:33:11 -0800

    remove suspend script, use batteryd instead

--- scripts/suspend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Check the output of acpi to determine if the battery is discharging and
-# whether the battery remaining is less than 5 percent. If both are true, then
-# suspend the system.
-#
-# Intended to run as a cron job every minute
-[[ $(/usr/bin/acpi | /usr/bin/awk '{exit !($3 == "Discharging," && $4 < 5)}') ]] &&
-    /usr/bin/systemctl "suspend"
