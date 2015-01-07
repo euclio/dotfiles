88a215928687c6c256710ad73a246993cba3e548
Andy Russell <arussell123@gmail.com>
2015-01-07 02:12:15 -0800

    use XDG directories in program environments

--- _config/bash/xdg.sh
+++ _config/bash/xdg.sh
@@ -1,3 +1,9 @@
 #1/bin/sh
 
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+export HISTFILE="$XDG_DATA_HOME/bash/history"
+export LESSHISTFILE="$XDG_DATA_HOME/less/history"
+export PIP_LOG_FILE="$XDG_DATA_HOME/pip/log"
+export PYLINTHOME="$XDG_DATA_HOME/pylint"
+export PYLINTRC="$XDG_CONFIG_HOME/pylint/pylintrc"
+export PYTHON_EGG_CACHE="$XDG_CACHE_HOME/python/egg-cache"
