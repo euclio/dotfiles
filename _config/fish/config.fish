d145d652c815d190f8ce63009225cd1773056754
Andy Russell <arussell123@gmail.com>
2015-01-26 11:56:14 -0800

    enable virtualfish autoactivation and aliases

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -1,3 +1,8 @@
 source $XDG_CONFIG_HOME/fish/colors.fish
+
+# Virtualfish
+set -g VIRTUALFISH_COMPAT_ALIASES       # Add virtualenvwrapper-like commands
 source $XDG_CONFIG_HOME/fish/scripts/virtualfish/virtual.fish
+source $XDG_CONFIG_HOME/fish/scripts/virtualfish/auto_activation.fish
+
 source $XDG_CONFIG_HOME/bash/aliases.sh
