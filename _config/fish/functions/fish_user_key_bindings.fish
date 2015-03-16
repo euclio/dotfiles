8ceabc8f103ddb2f6c55543ba25638fa28928c40
Andy Russell <arussell123@gmail.com>
2015-03-16 12:56:03 -0400

    add key binding to simulate replace-char

--- _config/fish/functions/fish_user_key_bindings.fish
+++ _config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,7 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
+
+    # Simulate 'r' in insert mode
+    bind -m replace-one r force-repaint
+    bind -M replace-one -m default '' delete-char force-repaint self-insert
 end
