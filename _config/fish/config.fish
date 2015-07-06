07902f7d6721e3ba458e8df44cb0802e16e7abd8
Andy Russell <arussell123@gmail.com>
2015-07-06 01:02:35 -0500

    use virtualfish python module instead of script

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -1,9 +1,7 @@
 source $XDG_CONFIG_HOME/fish/colors.fish
 
 # Virtualfish
-set -g VIRTUALFISH_COMPAT_ALIASES       # Add virtualenvwrapper-like commands
 set -g VIRTUALFISH_HOME $XDG_DATA_HOME/virtualenvs
-source $XDG_CONFIG_HOME/fish/scripts/virtualfish/virtual.fish
-source $XDG_CONFIG_HOME/fish/scripts/virtualfish/auto_activation.fish
+eval (python -m virtualfish auto_activation compat_aliases)
 
 source $XDG_CONFIG_HOME/bash/aliases.sh
