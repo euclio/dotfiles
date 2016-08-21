5059f2f4d566cbb17f732eebf37ee28ee4864d32
Andy Russell <arussell123@gmail.com>
2016-08-21 15:02:04 -0400

    fix(alias): read aliases from env folder

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -4,4 +4,4 @@ source $XDG_CONFIG_HOME/fish/colors.fish
 set -g VIRTUALFISH_HOME $XDG_DATA_HOME/virtualenvs
 eval (python -m virtualfish auto_activation compat_aliases)
 
-source $XDG_CONFIG_HOME/bash/aliases.sh
+source $XDG_CONFIG_HOME/env/aliases.sh
