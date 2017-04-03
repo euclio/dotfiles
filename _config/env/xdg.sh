1f06ff5d455a0f1ca9113f57864a9b1b96cd43b1
Andy Russell <arussell123@gmail.com>
2017-04-03 15:32:52 -0400

    refactor(xmonad): use XDG for xmonad

--- _config/env/xdg.sh
+++ _config/env/xdg.sh
@@ -6,3 +6,10 @@ export PIP_LOG_FILE="$XDG_DATA_HOME/pip/log"
 export PYLINTHOME="$XDG_DATA_HOME/pylint"
 export PYLINTRC="$XDG_CONFIG_HOME/pylint/pylintrc"
 export PYTHON_EGG_CACHE="$XDG_CACHE_HOME/python/egg-cache"
+export XMONAD_CONFIG_DIR="$XDG_CONFIG_HOME/xmonad"
+export XMONAD_CACHE_HOME="$XDG_CACHE_HOME/xmonad"
+export XMONAD_DATA_HOME="$XDG_DATA_HOME/xmonad"
+
+# xmonad will not create these directories on its own
+mkdir -p "$XMONAD_CACHE_HOME"
+mkdir -p "$XMONAD_DATA_HOME"
