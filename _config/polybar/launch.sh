242c99deb129c6636285252ee05b66708d83fffb
Andy Russell <arussell123@gmail.com>
2017-10-07 20:06:18 -0400

    feat(polybar): start a bar for each monitor

--- _config/polybar/launch.sh
+++ _config/polybar/launch.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate all bars already running
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
+# Start a bar for each monitor
+for monitor in $(xrandr -q | grep ' connected' | cut -d ' ' -f1); do
+  MONITOR=${monitor} polybar main &
+done
