ad7ba709c042731e72260bc821f235f45bb959a5
Andy Russell <arussell123@gmail.com>
2018-01-28 23:22:49 -0500

    style(aliases): group XDG-related aliases

--- _config/env/aliases.sh
+++ _config/env/aliases.sh
@@ -19,21 +19,17 @@ alias nicklist="cat $XDG_CONFIG_HOME/irssi/nicklistfifo"
 # Give words command history
 alias words='rlwrap words'
 
-# Workarounds to make programs obey the XDG base directory specification
-alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
-alias irssi="irssi --home $XDG_CONFIG_HOME/irssi"
-
 # Update all packages, including development packages
 alias pacu='pacaur -Syu --devel --needed --noedit'
-
-# Put SBT's config in the config directory, and the ivy cache in the cache
-alias sbt="sbt -Dsbt.global.base=$XDG_CONFIG_HOME/sbt -Dsbt.ivy.home=$XDG_CACHE_HOME/ivy"
-
-# Put latexmk's config in the config directory
-alias latexmk="latexmk -r $XDG_CONFIG_HOME/latexmk/latexmkrc"
 
 # Perforce
 alias pshelved="p4 changes -u $USER -s shelved"
 
 # Live life on the edge
 alias vim='nvim'
+
+# Workarounds to make programs obey the XDG base directory specification
+alias irssi="irssi --home $XDG_CONFIG_HOME/irssi"
+alias latexmk="latexmk -r $XDG_CONFIG_HOME/latexmk/latexmkrc"
+alias sbt="sbt -Dsbt.global.base=$XDG_CONFIG_HOME/sbt -Dsbt.ivy.home=$XDG_CACHE_HOME/ivy"
+alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
